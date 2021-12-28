7a6ff2bcdaec98493f3f30e91fddea25720c69e2
Enes izgi <enesizgi@tutanota.com>
2021-12-28 17:06:00 +0300

    First commit

---
+++ packages/435/the2/client.c
@@ -0,0 +1,9 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
---
+++ packages/435/the2/server.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    struct addrinfo test;
+    printf("%s\n",argv[3]);
+    return 0;
+}
