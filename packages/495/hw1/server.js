a15ab1853ffcc7b8a37a5ee85e71a166ce4a8e1b
Enes İzgi <enesizgi@tutanota.com>
2022-04-12 20:57:20 +0300

    first commit

---
+++ packages/495/hw1/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 4000;
+
+// Serve static React files
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
