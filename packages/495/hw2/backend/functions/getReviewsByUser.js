1faf0955e452c25d380aea81588c6e6a1d985988
Enes İzgi <enesizgi@tutanota.com>
2022-05-20 22:25:16 +0300

    revert sorting of reviewed books

--- packages/495/hw2/backend/functions/getReviewsByUser.js
+++ packages/495/hw2/backend/functions/getReviewsByUser.js
@@ -1,36 +1,18 @@
 exports = async function(arg){
   var collection = context.services.get("mongodb-atlas").db("data").collection("books");
   var docs;
-  // if (arg.skip > 0) {
-  //   if (arg.limit > 0) {
-  //     docs = await collection.find({ [`reviews.${context.user.id}`]: { $exists: true } }).skip(arg.skip).limit(arg.limit);
-  //   } else {
-  //     docs = await collection.find({ [`reviews.${context.user.id}`]: { $exists: true } }).skip(arg.skip);
-  //   }
-  // } else {
-  //   if (arg.limit > 0) {
-  //     docs = await collection.find({ [`reviews.${context.user.id}`]: { $exists: true } }).limit(arg.limit);
-  //   } else {
-  //     docs = await collection.find({ [`reviews.${context.user.id}`]: { $exists: true } });
-  //   }
-  // }
-
-  docs = await collection.find({ [`reviews.${context.user.id}`]: { $exists: true } });
-
-  var favDocs = docs.filter(doc => doc?.favoritedBy?.includes(context.user.id)).sort((a, b) => {
-    var x = a?.favTimestamps?.[context.user.id];
-    var y = b?.favTimestamps?.[context.user.id];
-    return y - x;
-  });
-
-  var reviewDocs = docs.filter(doc => !favDocs.find(i => i._id === doc._id)).sort((a, b) => {
-    var x = a?.ratings?.[context.user.id];
-    var y = b?.ratings?.[context.user.id];
-    return y - x;
-  });
-
-  var newDocs = [...favDocs, ...reviewDocs].slice(arg.skip, arg.skip + arg.limit);
-  return newDocs;
-
-  // return docs;
+  if (arg.skip > 0) {
+    if (arg.limit > 0) {
+      docs = await collection.find({ [`reviews.${context.user.id}`]: { $exists: true } }).skip(arg.skip).limit(arg.limit);
+    } else {
+      docs = await collection.find({ [`reviews.${context.user.id}`]: { $exists: true } }).skip(arg.skip);
+    }
+  } else {
+    if (arg.limit > 0) {
+      docs = await collection.find({ [`reviews.${context.user.id}`]: { $exists: true } }).limit(arg.limit);
+    } else {
+      docs = await collection.find({ [`reviews.${context.user.id}`]: { $exists: true } });
+    }
+  }
+  return docs;
 };
