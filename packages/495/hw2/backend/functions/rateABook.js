8e250441359a299920ab4aef7e5dbe770e2293b0
Enes İzgi <enesizgi@tutanota.com>
2022-05-20 04:09:31 +0300

    Merge branch 'master' of github.com:enesizgi/cloud-hw2

--- packages/495/hw2/backend/functions/rateABook.js
+++ packages/495/hw2/backend/functions/rateABook.js
@@ -1,15 +1,11 @@
-exports = async function(){
-  var arg = {bookID: '62864bc90dc76f568f17ed2d', rating: 5};
+exports = async function(arg){
   var collection = context.services.get("mongodb-atlas").db("data").collection("books");
   var bookID = new BSON.ObjectId(arg.bookID);
   var book = await collection.findOne({_id: bookID});
   var ratings = book.ratings || {};
+  ratings[context.user.id] = arg.rating;
   
-  ratings["6286848dd0df270d4c3794d6"] = arg.rating;
   var newBook = {...book, ratings};
-  // return newBook;
-  
   await collection.updateOne({_id: bookID}, {$set: {ratings: ratings}});
   return newBook;
-  
 };
