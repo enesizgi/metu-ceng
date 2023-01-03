654608be66de8366015abdf9c75ed9498c13855f
Enes izgi <enesizgi@pm.me>
2023-01-03 22:25:36 +0300

    part1 completed, may adjust parameters later

--- packages/466/the4/main.py
+++ packages/466/the4/main.py
@@ -6,49 +6,53 @@ INPUT_PATH = "./THE4_Images/"
 OUTPUT_PATH = "./Outputs/"
 
 
+def count_flowers(image, threshold, erode_iterations=1, dilate_iterations=1):
+    image = cv.GaussianBlur(image, (5, 5), 0)
+
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+    ret, thresh = cv.threshold(gray, threshold, 255, cv.THRESH_BINARY_INV)
+
+    kernel = np.ones((5, 5), np.uint8)
+    opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=2)
+
+    mask = cv.erode(opening, kernel, iterations=erode_iterations)
+    mask = cv.dilate(mask, kernel, iterations=dilate_iterations)
+
+    mask_white = cv.bitwise_not(mask)
+
+    contours, hierarchy = cv.findContours(mask_white, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv.contourArea, reverse=True)
+    avg_area = 0
+    for contour in contours:
+        area = cv.contourArea(contour)
+        avg_area += area
+    avg_area /= len(contours)
+    filtered_contours = []
+    for contour in contours:
+        area = cv.contourArea(contour)
+        if area > avg_area * 0.4:
+            filtered_contours.append(contour)
+    print(len(filtered_contours))
+
+    return mask_white
+
+
 def main():
     if not os.path.exists(OUTPUT_PATH):
         os.makedirs(OUTPUT_PATH)
 
-    image = cv.imread(INPUT_PATH + "A3.png")
-    # cv.imwrite(OUTPUT_PATH + 'A1.png', image)
+    a1 = cv.imread(INPUT_PATH + "A1.png")
+    a1 = count_flowers(a1, erode_iterations=2, dilate_iterations=2, threshold=80)
+    cv.imwrite(OUTPUT_PATH + "A1.png", a1)
 
-    # Convert the image to grayscale
-    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    a2 = cv.imread(INPUT_PATH + "A2.png")
+    a2 = count_flowers(a2, erode_iterations=25, dilate_iterations=25, threshold=110)
+    cv.imwrite(OUTPUT_PATH + "A2.png", a2)
 
-    # Apply Otsu's thresholding to create a binary image
-    ret, thresh = cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
-
-    # Use morphological transformations to remove small holes and noise from the image
-    kernel = np.ones((5, 5), np.uint8)
-    opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=2)
-
-    # Create a mask to separate the flowers from the background
-    # mask = cv.dilate(opening, kernel, iterations=1)
-    mask = cv.dilate(opening, kernel, iterations=45)
-    mask = cv.erode(mask, kernel, iterations=45)
-
-    # Use the mask to separate the flowers from the background in the input image
-    # result = cv.bitwise_and(image, image, mask=mask)
-    # result_gray = cv.cvtColor(result, cv.COLOR_BGR2GRAY)
-    # ret, thresh = cv.threshold(result, 0, 10, 0)
-    cv.imwrite('result2.png', mask)
-    labeled_image = np.zeros(mask.shape)
-    x = cv.connectedComponentsWithAlgorithm(mask, connectivity=4, ccltype=cv.CCL_WU, ltype=cv.CV_32S)
-    # result = cv.bitwise_not(result)
-
-    # cv.imwrite('result3_result.jpg', result)
-    # contours, hierarchy = cv.findContours(result_gray, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    # contours = sorted(contours, key=cv.contourArea, reverse=True)
-    # for contour in contours:
-    #     (x, y), radius = cv.minEnclosingCircle(contour)
-    #     center = (int(x), int(y))
-    #     radius = int(radius)
-    #     cv.circle(image, center, radius, (0, 255, 0), 2)
-        # x, y, w, h = cv.boundingRect(contour)
-        # cv.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), image.shape[0] // 200)
-    # Save the result
-    cv.imwrite('result3.jpg', image)
+    a3 = cv.imread(INPUT_PATH + "A3.png")
+    a3 = count_flowers(a3, erode_iterations=10, dilate_iterations=10, threshold=130)
+    cv.imwrite(OUTPUT_PATH + "A3.png", a3)
 
 
 if __name__ == '__main__':
